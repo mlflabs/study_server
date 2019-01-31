1cd7fcc36ab99cfc6318c4c89d55159a440938e3
mike <mlflabs@hotmail.com>
2019-01-31 16:30:07 +0800

    added pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'studyapi',
+    script: './src/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '100M',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
